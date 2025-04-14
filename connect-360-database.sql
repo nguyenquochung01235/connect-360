7de30d3af699489948fe25e8f2c8d9eb6d2e9e1e
Nguyen Quoc Hung (Anthony) <nguyenquochung01235@gmail.com>
2025-04-14 22:24:56 +0700

    add db for import

--- connect-360-database.sql
+++ connect-360-database.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `connect-360-database`
+-- Database: `connect_360_database`
 --
 
 -- --------------------------------------------------------
